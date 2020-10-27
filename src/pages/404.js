f5a1ee8a8bd8f0f1beef04082d51518db5bac277
kim-sardine <kim.sardine@gmail.com>
2020-10-27 22:41:52 +0900

    Update 404 design

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
+import styled from 'styled-components';
+
+const Center = styled.div`
+  height: calc(100vh - 25rem);
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 export default () => (
   <Layout>
     <SEO title="404: Not found" location="/404" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Center>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Center>
   </Layout>
 );
